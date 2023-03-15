86425ae2a5b5f062563e3ebf2dcc291d430613c4
AnujPandey007 <panuj330@gmail.com>
2023-03-15 15:48:24 +0530

    added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,11 +20,11 @@ export default function Navbar({isAuth, setIsAuth}) {
 
   return (
     
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
             <Link to="/" className="flex items-center">
                 <img src="https://media.licdn.com/dms/image/C4D0BAQHhJ_TEVwqDwA/company-logo_200_200/0/1630397378021?e=1686787200&v=beta&t=gL44LU_HPbJV_guAs_CqVTy5ujWiKackwpD32DiGDi0" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
-                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">OneShot</span>
+                <span className="self-center text-xl font-semibold whitespace-nowrap">OneShot</span>
             </Link>
 
             <div className="block w-auto" id="navbar-default">
@@ -33,7 +33,7 @@ export default function Navbar({isAuth, setIsAuth}) {
                   <Link to="/" className="block py-2 px-6 bg-white text-black" aria-current="page">Home</Link>
                 </li>}
                 {isAuth && <li>
-                  <Link onClick={handleLogout} className="block py-2 text-gray-400 hover:text-black dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 dark:hover:bg-transparent">Logout</Link>
+                  <Link onClick={handleLogout} className="block py-2 text-gray-400 hover:text-black">Logout</Link>
                 </li>}
               </ul>
             </div>
